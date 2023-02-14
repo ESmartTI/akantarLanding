dcb85cc82d83cc97a988729bbf2ee9d265de3ceb
Esteban Martínez <cestebanmq@gmail.com>
2023-02-13 23:34:44 -0500

    Update funciones.js
    Agrego comentarios a las funciones

--- APIs/consultarTextos/funciones.js
+++ APIs/consultarTextos/funciones.js
@@ -1,5 +1,6 @@
-Prueba
-
+/**
+ * Consulta la maestra de textos y devuelve un arreglo de textos Clave:Valor.
+ */
 function consultarTextosHome(){
   var libro = SpreadsheetApp.openById("1Pq435PUXoI_JWdzTaYwV1OpZKO6nElWXh8pHc2ub61w");
   var textos = libro.getRange("textos!A1:B").getValues();
@@ -7,6 +8,9 @@ function consultarTextosHome(){
   return textos;
 }
 
+/**
+ * Función de prueba que consulta los textos de la maestra y los pega en una nueva hoja. Solo válida para probar, no tiene funcionalidad real.
+ */
 function usarTextos(){
     var libro = SpreadsheetApp.openById("1Pq435PUXoI_JWdzTaYwV1OpZKO6nElWXh8pHc2ub61w");
     var textos = consultarTextosHome();
