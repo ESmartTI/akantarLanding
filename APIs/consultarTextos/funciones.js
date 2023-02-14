cb007160db61af1fa56ea920cd25235d3299f7ba
Esteban Martínez <cestebanmq@gmail.com>
2023-02-13 23:21:06 -0500

    Implementación de las funciones consultarTextosHome y usarTextos
    La función toma los datos desde GoogleSheets y los devuelve como un arreglo

--- APIs/consultarTextos/funciones.js
+++ APIs/consultarTextos/funciones.js
@@ -1,3 +1,15 @@
+Prueba
+
 function consultarTextosHome(){
-    Logger.log("Hola mundo")
+  var libro = SpreadsheetApp.openById("1Pq435PUXoI_JWdzTaYwV1OpZKO6nElWXh8pHc2ub61w");
+  var textos = libro.getRange("textos!A1:B").getValues();
+  Logger.log(textos[0][1]);
+  return textos;
+}
+
+function usarTextos(){
+    var libro = SpreadsheetApp.openById("1Pq435PUXoI_JWdzTaYwV1OpZKO6nElWXh8pHc2ub61w");
+    var textos = consultarTextosHome();
+    var rango = libro.getRange("hojaPruebas!A1:B")
+    rango.setValues(textos);
 }
